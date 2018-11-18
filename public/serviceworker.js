a2c7b4e9e8cbf811ad03dd94dcc275ed7b65fe24
Your Name <you@example.com>
2018-11-18 16:18:17 -0500

    ch4

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "gih-cache";
+var CACHE_NAME = "gih-cache-v5";
 var CACHED_URLS = [
   "/index-offline.html",
   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css",
@@ -7,24 +7,38 @@ var CACHED_URLS = [
   "/img/logo-header.png"
 ];
 
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.addAll(CACHED_URLS);
     })
   );
 });
 
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (res) {
+        if (res) {
+          return (res);
+        } else if (event.request.headers.get("accept").includes("text/html")){
           return caches.match("/index-offline.html");
         }
       });
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (CACHE_NAME !== cacheName && cacheName.startsWith("gih-cache")) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
   );
 });
